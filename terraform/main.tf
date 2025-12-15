fe13103f137f93009bdf54ceaf1fa09f3c2ad9aa
1vmc1 <vmc0@mail.ru>
2025-12-15 18:16:08 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,66 @@
+terraform {
+  required_version = ">= 1.3.0"
 
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.107.0"
+    }
+  }
+}
+
+provider "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+  token     = var.yc_token
+}
+
+resource "yandex_vpc_network" "this" {
+  name = "${var.project_name}-network"
+}
+
+resource "yandex_vpc_subnet" "this" {
+  name           = "${var.project_name}-subnet"
+  zone           = var.zone
+  network_id     = yandex_vpc_network.this.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
+}
+
+resource "yandex_compute_instance" "vm" {
+  name        = "${var.project_name}-vm"
+  platform_id = "standard-v3"
+  zone        = var.zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = 20
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.this.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key)}"
+  }
+}
+
+output "vm_external_ip" {
+  value       = yandex_compute_instance.vm.network_interface.0.nat_ip_address
+  description = "External IP of the VM"
+}
+
+output "vm_internal_ip" {
+  value       = yandex_compute_instance.vm.network_interface.0.ip_address
+  description = "Internal IP of the VM"
+}
