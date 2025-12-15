4c33fac392dcbc673832215af36f1bb52e44b47d
1vmc1 <vmc0@mail.ru>
2025-12-15 18:17:35 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,38 @@
+variable "cloud_id" {
+  type        = string
+  description = "Yandex Cloud ID"
+  default     = "b1gp31d2a5js0d286632"
+}
 
+variable "folder_id" {
+  type        = string
+  description = "Yandex Cloud Folder ID"
+  default     = "b1gslb67oosr9ejfib5i"
+}
+
+variable "zone" {
+  type        = string
+  default     = "ru-central1-d"
+  description = "Yandex Cloud zone"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "yc-lab5"
+  description = "Project name prefix"
+}
+
+variable "image_id" {
+  type        = string
+  description = "ID образа в Yandex Cloud (например Ubuntu 22.04)"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Path to SSH public key file"
+}
+
+variable "yc_token" {
+  type        = string
+  description = "Yandex Cloud API token"
+}
